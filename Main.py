22bc817901763cb4117c22ef16d08ca0bdc4e369
Shahmeer0311 <22alis@students.watfordboys.org>
2025-01-18 14:24:52 +0000

    e

---
+++ Main.py
@@ -0,0 +1,10 @@
+T1 = (1,"john",3.6,False)
+print(T1)
+T2 = (1,6,5,98,76,1089,456)
+print(T2)
+T2 = T2 + (9,)
+print(T2)
+T3 = (3,7,87,65,91,3,45,3,3,67,5)
+print(T3.count(3))
+slice = T3[2:7:2]
+print(slice)
